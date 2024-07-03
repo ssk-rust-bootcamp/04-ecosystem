1b520d1f83de331e999f13313f27b97c2861cc5a
shikun.sh <shikun.sh@cheilpengtai.com>
2024-07-03 17:33:14 +0800

    v4-2-tracing

--- examples/err.rs
+++ examples/err.rs
@@ -1,7 +1,29 @@
-use std::{fs, mem::size_of};
-
 use anyhow::Context;
-use ecosystem::MyError;
+use std::{fs, mem::size_of};
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum MyError {
+    #[error("I/O Error: {0}")]
+    Io(#[from] std::io::Error),
+    #[error("Param Error: {0}")]
+    Param(#[from] std::num::ParseIntError),
+    #[error("Serialize Error: {0}")]
+    Serialize(#[from] serde_json::Error),
+    #[error("Big Error: {0:?}")]
+    BigError(Box<BigError>),
+    #[error("Custom Error: {0}")]
+    Custom(String),
+}
+#[allow(unused)]
+#[derive(Debug)]
+pub struct BigError {
+    a: String,
+    b: Vec<String>,
+    c: [u8; 64],
+    d: u64,
+}
+
 fn main() -> Result<(), anyhow::Error> {
     println!("size of anyhow::Error:{}", size_of::<anyhow::Error>());
     println!("size of std::io:Error:{}", size_of::<std::io::Error>());
