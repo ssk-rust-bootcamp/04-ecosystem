c136a4ba352620be7dca237b751ee585dd0bcd3c
shikun.sh <shikun.sh@cheilpengtai.com>
2024-07-04 17:03:38 +0800

    v4-3-opentelemetry

--- examples/axum_tracing.rs
+++ examples/axum_tracing.rs
@@ -1,7 +1,15 @@
 use std::time::Duration;
 
 use axum::routing::get;
+use opentelemetry::KeyValue;
+use opentelemetry_otlp::WithExportConfig;
+use opentelemetry_sdk::{
+    runtime,
+    trace::{self, RandomIdGenerator, Tracer},
+    Resource,
+};
 use tokio::{
+    join,
     net::TcpListener,
     time::{sleep, Instant},
 };
@@ -28,7 +36,14 @@ async fn main() -> anyhow::Result<()> {
         .pretty()
         .with_filter(LevelFilter::INFO);
 
-    tracing_subscriber::registry().with(console).with(file).init();
+    let tracer = init_tracker()?;
+    let opentelemetry = tracing_opentelemetry::layer().with_tracer(tracer);
+
+    tracing_subscriber::registry()
+        .with(console)
+        .with(file)
+        .with(opentelemetry)
+        .init();
     let addr = "0.0.0.0:8080";
     let app = axum::Router::new().route("/", get(index_handler));
     let listener = TcpListener::bind(addr).await?;
@@ -49,8 +64,50 @@ async fn index_handler() -> &'static str {
 #[instrument]
 async fn long_task() -> &'static str {
     let start = Instant::now();
-    sleep(Duration::from_millis(100)).await;
+    let s1 = sleep(Duration::from_millis(11));
+
+    // spawn multiple tasks
+
+    let t1 = task1();
+    let t2 = task2();
+    let t3 = task3();
+    join!(s1, t1, t2, t3);
     let elapsed = start.elapsed().as_millis() as u64;
     warn!(app.task_duration = elapsed, "task takes too long");
     "Hello, World!"
+}
+
+#[instrument]
+async fn task1() {
+    sleep(Duration::from_millis(10)).await;
+}
+
+#[instrument]
+async fn task2() {
+    sleep(Duration::from_millis(50)).await;
+}
+
+#[instrument]
+async fn task3() {
+    sleep(Duration::from_millis(30)).await;
+}
+
+fn init_tracker() -> anyhow::Result<Tracer> {
+    let tracer = opentelemetry_otlp::new_pipeline()
+        .tracing()
+        .with_exporter(
+            opentelemetry_otlp::new_exporter()
+                .tonic()
+                .with_endpoint("http://localhost:58799"),
+        )
+        .with_trace_config(
+            trace::config()
+                .with_id_generator(RandomIdGenerator::default())
+                .with_max_events_per_span(32)
+                .with_max_attributes_per_event(64)
+                .with_resource(Resource::new(vec![KeyValue::new("service.name", "axum-tracing")])),
+        )
+        .install_batch(runtime::Tokio)?;
+
+    Ok(tracer)
 }
