8eb200046d54f33846beb75aad08b2a50fea820c
shikun.sh <shikun.sh@cheilpengtai.com>
2024-07-08 09:25:38 +0800

    v4-9-tokio

---
+++ examples/tokio1.rs
@@ -0,0 +1,37 @@
+use std::{
+    thread::{self},
+    time::Duration,
+};
+
+use tokio::{
+    fs,
+    runtime::{Builder, Runtime},
+    time::sleep,
+};
+
+fn main() {
+    let handle = thread::spawn(|| {
+        let rt = Builder::new_current_thread().enable_all().build().unwrap();
+        rt.block_on(run(&rt));
+    });
+    handle.join().unwrap();
+}
+
+fn expensive_blocking_task(s: String) -> String {
+    thread::sleep(std::time::Duration::from_millis(500));
+    blake3::hash(s.as_bytes()).to_string()
+}
+
+async fn run(rt: &Runtime) {
+    rt.spawn(async {
+        println!("future 1");
+        let content = fs::read("../Cargo.toml").await.unwrap();
+        println!("content : {:?}", content.len());
+    });
+    rt.spawn(async {
+        println!("future 2");
+        let result = expensive_blocking_task("hello".to_string());
+        println!("result : {}", result);
+    });
+    sleep(Duration::from_secs(1)).await;
+}
---
+++ examples/tokio2.rs
@@ -0,0 +1,38 @@
+use std::{thread, time::Duration};
+
+use tokio::sync::mpsc;
+
+#[tokio::main]
+async fn main() {
+    let (tx, rx) = mpsc::channel(32);
+    let handle = woker(rx);
+
+    tokio::spawn(async move {
+        let mut i = 0;
+        loop {
+            i += 1;
+            println!("Sending task {}", i);
+            tx.send(format!("task {i}")).await.unwrap();
+        }
+    });
+
+    handle.join().unwrap();
+}
+fn expensive_blocking_task(s: String) -> String {
+    thread::sleep(Duration::from_millis(800));
+    blake3::hash(s.as_bytes()).to_string()
+}
+fn woker(mut rx: mpsc::Receiver<String>) -> thread::JoinHandle<()> {
+    thread::spawn(move || {
+        let (sender, receiver) = std::sync::mpsc::channel();
+        while let Some(s) = rx.blocking_recv() {
+            let sender_clone = sender.clone();
+            thread::spawn(move || {
+                let ret = expensive_blocking_task(s);
+                sender_clone.send(ret).unwrap();
+            });
+            let result = receiver.recv().unwrap();
+            println!("result :{}", result);
+        }
+    })
+}
